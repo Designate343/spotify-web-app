b107490fcd356e9e07e121854a55d949faed0e4f
Ed Steele <edmundsteele@gmail.com.com>
2020-10-25 14:58:31 +0000

    adding some css and imporving audio playback

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ var spotifyTopTracks = new Vue({
 
         },
         genres: [],
-        timeframe: 'medium_term'
+        timeframe: 'medium_term',
+        playingTrack : null, //todo refactor to own component/vue thing
+        trackPlaying: false
     },
     methods: {
         downloadTopTracks: function () {
@@ -67,11 +69,9 @@ var spotifyTopTracks = new Vue({
         },
 
         getMostPopularGenres() {
-            //need to lookup artists to get most popular genres of these popular tracks
             let artistIds = new Set();
 
             let tracksResponse = this.tracksCache[this.timeframe]['raw'];
-            console.log(tracksResponse);
             tracksResponse.forEach(track => {
                 let artists = track['artists'];
                 artists.forEach(artist => {
@@ -118,105 +118,60 @@ var spotifyTopTracks = new Vue({
                     this.tracksCache[this.timeframe]['genreHistogram'] = topFifteen;
                     this.genres = topFifteen;
                 });
-        }
-    }
-});
+        },
 
-//TODO: sort all this stuff
-var search = new Vue({
-    el: '#do-search',
-    data: {
-        searchString: "",
-        tracks: []
-    },
-    methods: {
-        searchForTrackName: function () {
-            var url = 'https://api.spotify.com/v1/search';
-            let type = 'track';
-
-            var urlSearchParams = new URLSearchParams('market=GB');
-            urlSearchParams.set('type', type);
-            //TODO: not updating if search is replaced
-            urlSearchParams.set('q', this.searchString);
-            urlSearchParams.set('limit', 10);
-
-            //reset tracks
-            this.tracks = [];
-
-            getJsonResponse(url + '?' + urlSearchParams.toString())
-                .then(json => json['tracks'])
-                .then(tracks => tracks['items'])
-                .then(items => items.forEach(element => {
-                    let name = element['name'];
-                    let artists = element['artists'];
-                    //only use first artist even if there are 2
-                    let artistName = artists[0]['name'];
-                    let albumName = element["album"]["name"];
-                    let id = element['id'];
-
-                    let trackString = name + " " + artistName + " " + albumName;
-                    this.tracks.push({
-                        'text': trackString,
-                        'id': id
-                    });
-                    //each item must be clickable to generate some recomendations based on it
-                }));
-        }
-    }
-});
-
-var getRecomendations = new Vue({
-    el: '#get-recomendations',
-    data: {
-        trackId: '',
-        tracks: [
-        ]
-    },
-    methods: {
-        //slightly strange that this adds the recomendations tracks in place - makes more sense to 
-        //search just for one track surely - and display results somewhere else? 
-        generateRecomendationsForTrack(trackId) {
-            var url = 'https://api.spotify.com/v1/recommendations';
-            console.log(trackId);
-
-            var urlSearchParams = new URLSearchParams('market=GB');
-            urlSearchParams.set('seed_tracks', trackId);
-            urlSearchParams.set('limit', 20);
-
-            var trackIndex = 0;
-            for (let index = 0; index < this.tracks.length; index++) {
-                const track = this.tracks[index];
-                if (trackId === track['id']) {
-                    if (this.tracks[index].recomendations === undefined) {
-                        this.tracks[index].recomendations = []
-                    }
-                    trackIndex = index;
-                }
+        play(track) {
+            if (track.preview_url == null) {
+                alert('This track does not have a preview link:(');
+                console.error('Track ' + track.trackId + " does not have an mp3 preview url")
+                console.error(track);
+                return;
             }
+            if (this.trackPlaying) {
+                alert('Pause or let the current track finish playing before starting another (your ears will thank you)')
+                return;
+            }
+            var trackIndex = findTrackIndex(track, this.tracks);
+            var dupTrack = track;
+            dupTrack.isPlaying = true;
+            var audio;
+            if (track.audio != null) {
+                //track has been played recently so resume from where it left off
+                audio = track.audio;
+            } else {
+                audio = new Audio(track.preview_url);
+                audio.addEventListener("ended", () => this.trackPlaying = false);
+            }
+            dupTrack.audio = audio;
+            audio.play();
 
-            getJsonResponse(url + '?' + urlSearchParams.toString(), requestParams)
-                .then(json => json['tracks'])
-                .then(items => {
-                    var recomendations = []
-                    items.forEach(element => {
-                        let name = element['name'];
-                        let artists = element['artists'];
-                        let artistName = artists[0]['name'];
-                        let albumName = element["album"]["name"];
-                        let id = element['id'];
-
-                        let trackString = name + " " + artistName + " " + albumName;
-
-                        recomendations.push({
-                            'text': trackString,
-                            'id': id
-                        });
-                    });
-                    var newValue = this.tracks[trackIndex];
-                    newValue.recomendations = recomendations;
-                    //needs to do this special setting to update component https://vuejs.org/v2/guide/reactivity.html#For-Arrays 
-                    getRecomendations.$set(getRecomendations.tracks, trackIndex, newValue);
-                });
+            spotifyTopTracks.$set(spotifyTopTracks.tracks, trackIndex, dupTrack);
+            this.trackPlaying = true; //global variable to stop many tracks playing at the same time
+        },
+        pause(track) {
+            if (track.preview_url == null) {
+                return;
+            }
+            if (track.isPlaying) {
+                var audio = track.audio;
+                audio.pause();
+                var trackIndex = findTrackIndex(track, this.tracks);
+                var dupTrack = track;
+                dupTrack.isPlaying = false;
+                this.trackPlaying = false;
+                spotifyTopTracks.$set(spotifyTopTracks.tracks, trackIndex, dupTrack);
+            }
         }
     }
 });
+
+function findTrackIndex(trackId, tracks) {
+    var trackIndex = -1;
+    for (let index = 0; index < tracks.length; index++) {
+        const element = tracks[index];
+        if (trackId = element.track_id) {
+            trackIndex = index;
+        }
+    }
+    return trackIndex;
+};
